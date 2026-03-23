e6018c5dc6bb37737ef7f57370a0a3310f20bdb2
Mazen Touati <mazen_touati@hotmail.com>
2026-03-23 20:01:20 +0100

    revert "fix: attempt to fix folder deletion in windows"
    This reverts commit 139d1ad305f65ee2f6c70084057f86793d64fdc2.

--- .aion/Engine/Operations/DeleteFolderOperation.php
+++ .aion/Engine/Operations/DeleteFolderOperation.php
@@ -20,29 +20,9 @@ class DeleteFolderOperation implements OperationContract
 
     public function execute(FilesystemOperator $filesystem): void
     {
-        $this->deleteRecursively($filesystem, $this->path);
-    }
-
-
-    private function deleteRecursively(FilesystemOperator $filesystem, string $path): void
-    {
-        if (! $filesystem->directoryExists($path)) {
-            return;
+        if ($filesystem->directoryExists($this->path)) {
+            $filesystem->deleteDirectory($this->path);
         }
-
-        // First delete the folder content
-        // to support deletion cross platforms when the script is running from the directory.
-        foreach ($filesystem->listContents($path, deep: false) as $item) {
-            if ($item->isDir()) {
-                $this->deleteRecursively($filesystem, $item->path());
-
-                continue;
-            }
-
-            $filesystem->delete($item->path());
-        }
-
-        $filesystem->deleteDirectory($path);
     }
 
     public function validate(FilesystemOperator $filesystem): void
